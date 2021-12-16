297b2b2e92310f2d32584fb8cd2eddba87b6fccf
Johnathan Cassady <jcassady@Johnathans-MBP.hsd1.wa.comcast.net>
2021-12-15 16:15:12 -0800

    fixed paths for py_con and eigenbot

--- build/eigenbot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/biorobotics/titanCode/src/eigenbot
-# Build directory: /home/biorobotics/titanCode/build/eigenbot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/eigenbot/eigenbot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/biorobotics/titanCode/src/eigenbot/eigenbot
-# Build directory: /home/biorobotics/titanCode/build/eigenbot/eigenbot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
